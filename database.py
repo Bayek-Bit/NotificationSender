81945b2f010588d3c1ca841a3cc849e7cfe991a3
Bayek-bit <tiwnk.7w1nk@yandex.ru>
2025-11-26 20:22:48 +0500

    use of Notification model as DTO add get_notifications_to_send add mark_notification_as_sent

--- database.py
+++ database.py
@@ -2,23 +2,19 @@ import asyncio
 from datetime import datetime, timezone
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from models import Base
+from models import Base, Notification
 from config import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def schedule_notification(
-        user_id: int,
-        message: str,
-        send_at: datetime
-    ) -> int | None:
+async def schedule_notification(notification: Notification) -> int | None:
 
-    if send_at.tzinfo is None:
+    if notification.send_at.tzinfo is None:
         send_at = send_at.replace(tzinfo=timezone.utc)
 
-    if send_at <= datetime.now(timezone.utc):
+    if notification.send_at <= datetime.now(timezone.utc):
         raise ValueError("Send time(send_at) should be in future")
 
     query = text("""
@@ -32,18 +28,53 @@ async def schedule_notification(
             result = await session.execute(
                 query,
                 {
-                    "user_id": user_id,
-                    "message": message,
-                    "send_at": send_at
+                    "user_id": notification.user_id,
+                    "message": notification.message,
+                    "send_at": notification.send_at
                 }
             )
             return result.scalar_one()
 
+async def get_notifications_to_send():
+    """Get notifications to send from datetime now"""
+    now = datetime.now(timezone.utc)
+    # Пока не совсем атомарно)
+    query = text("""
+        SELECT * FROM scheduled_notifications
+        WHERE send_at <= (:now)
+        AND status = 'pending'
+    """)
+    async with AsyncSessionLocal() as session:
+        result = await session.execute(
+            query,
+            {
+                "now": now
+            }
+        )
+        return result.mappings().all()
+
+async def mark_notification_as_sent(notification: Notification):
+    query = text("""
+        UPDATE TABLE scheduled_notifications
+        SET status = 'sent'
+        WHERE id = (:notification_id) AND user_id = (:user_id)
+    """)
+
+    async with AsyncSessionLocal() as session:
+        await session.execute(
+            query,
+            {
+                "notification_id": notification.id,
+                "user_id": notification.user_id
+            }
+        )
+        return True
 
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-        await schedule_notification(1, "Hello", datetime(2025, 11, 26))
+        a = await get_notifications_to_send()
+        print(a)
 
 # Запуск
 asyncio.run(create_tables())
